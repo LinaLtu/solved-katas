2686256db4a168a3d19a4cd367b0c7f144a46ff3
LinaLtu <lsuodyte@babbel.com>
2020-05-09 13:58:32 +0200

    add PriorityQueueClass and StackClass exercises

---
+++ data-structures/PriorityQueueClass.js
@@ -0,0 +1,39 @@
+// https://www.freecodecamp.org/learn/coding-interview-prep/data-structures/create-a-priority-queue-class
+
+function PriorityQueue() {
+  this.collection = [];
+  this.printCollection = function() {
+    console.log(this.collection);
+  };
+
+  this.enqueue = elemArray => {
+    let priorityIndex = elemArray[1];
+    let elemIndex = 0;
+    let continueLoop = true;
+
+    while (continueLoop) {
+      if (
+        elemIndex === this.collection.length ||
+        priorityIndex < this.collection[elemIndex][1]
+      ) {
+        this.collection.splice(elemIndex, 0, elemArray);
+        continueLoop = false;
+      }
+      elemIndex++;
+    }
+  };
+
+  this.dequeue = () => {
+    if (this.size() === 0) {
+      return;
+    }
+
+    return this.collection.shift();
+  };
+
+  this.size = () => this.collection.length;
+
+  this.front = () => this.collection[this.collection.length - 1];
+
+  this.isEmpty = () => Boolean(this.collection.length === 0);
+}
---
+++ data-structures/StackClass.js
@@ -0,0 +1,20 @@
+// https://www.freecodecamp.org/learn/coding-interview-prep/data-structures/create-a-stack-class
+
+function Stack() {
+  let collection = [];
+  this.print = () => {
+    console.log(collection);
+  };
+
+  this.push = elem => collection.push(elem);
+
+  this.pop = () => collection.pop();
+
+  this.peek = () => collection[0];
+
+  this.isEmpty = () => Boolean(collection.length === 0);
+
+  this.clear = () => (collection = []);
+
+  this.print = () => console.log(collection);
+}
